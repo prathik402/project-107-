6188b34f734bd2769ee953cf64dca237fef4ba2f
prathik402 <73998548+prathik402@users.noreply.github.com>
2023-05-06 12:23:42 +0530

    Update obj_trk_final.py

--- obj_trk_final.py
+++ obj_trk_final.py
@@ -44,31 +44,32 @@ def drawBox(img,bbox):
     x,y,w,h = int(bbox[0]),int(bbox[1]),int(bbox[2]),int(bbox[3])
     cv2.rectangle(img,(x,y),((x+w),(y+h)),(255,0,255),3,1)
     cv2.putText(img,"Tracking",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,255,0),2)
-if success:
-    drawBox(img,bbox)
-else:
-    cv2.putText(img,"Lost",(75,90),cv2.HERSHEY_SIMPLEX,0.7,(0,0,255),2)
-
+    
 while True:
-    check,img = video.read()
+    
+    check,img = video.read()   
     success,bbox = tracker.update(img)
 
+    if success:
+        drawBox(img,bbox)
+    else:
+        cv2.putText(img,"Lost",(75,90),cv2.FONT_HERSHEY_SIMPLEX,0.7,(0,0,255),2)
+        
+    goal_track(img,bbox)
 
-goal_track(img,bbox)
+    cv2.imshow("result",img)
+            
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        print("Closing")
+        break     
+
+
+
+)
     
-def drawBox(img,bbox):
-    #write code for this function here
-    pass
 
 
-while True:
-   #Write the code inside loop here
-   pass
-cv2.imshow(img)
 
-key = cv2.waitKey(1)
-if key == ord('q'):
-    print("Closing")
-    break
 video.release()
 cv2.destroyALLwindows() 
